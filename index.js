ae659094f7072f9cdc8c119794469145335ae5de
Victor Alves <victoralvescabral7@gmail.com>
2023-06-23 12:59:23 -0300

    Fourth Commit

--- index.js
+++ index.js
@@ -1,58 +1,60 @@
 let isModalOpen = false;
 let contrastToggle = false;
-const scaleFactor = 1/20
+const scaleFactor = 1 / 20;
 
 function moveBackground(event) {
-    const shapes = document.querySelectorAll(".shape")
-    const x = event.clientX * scaleFactor;
-    const y = event.clientY * scaleFactor;
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
 
-    for (let i = 0; i < shapes.length; i++) {
-        const isOdd = i % 2 !== 0;
-        const boolInt = isOdd ? -1 : 1;
-        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px) rotate(${x * boolInt * 10}deg)`
-    }
+  for (let i = 0; i < shapes.length; i++) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${
+      y * boolInt
+    }px) rotate(${x * boolInt * 10}deg)`;
+  }
 }
 
 function toggleContrast() {
-    contrastToggle = !contrastToggle
-    
-    if(contrastToggle) {
-        document.body.classList += "dark-theme" 
-    }
-    else {
-        document.body.classList.remove("dark-theme")
-    }
+  contrastToggle = !contrastToggle;
+
+  if (contrastToggle) {
+    document.body.classList += "dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
 }
 
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const sucess = document.querySelector(".modal__overlay--sucess");
+  loading.classList += " modal__overlay--visible";
 
-function contact (event) {
-    event.preventDefault();
-    const loading = document.querySelector('.modal__overlay--loading')
-    const sucess = document.querySelector('.modal__overlay--sucess')
-    loading.classList += " modal__overlay--visible"
-    
-    emailjs
-          .sendForm(
-                'service_ft79hpl',
-                'template_zql1khp',
-                event.target,
-                'YIVHwcnxzWc5rEOtr'
-            ).then(() => {
-                loading.classList.remove("modal__overlay--visible")
-                sucess.classList += " modal__overlay--visible"
-            }).catch(() => {
-                loading.classList.remove("modal__overlay--visible")
-                alert(
-                    "The email service is temporarily unavailable. Please contact me directly on victoralvescabral7@gmail.com"
-                )
-            })
+  emailjs
+    .sendForm(
+      "service_ft79hpl",
+      "template_zql1khp",
+      event.target,
+      "YIVHwcnxzWc5rEOtr"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      sucess.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly on victoralvescabral7@gmail.com"
+      );
+    });
 }
 function toggleModal() {
-    if (isModalOpen) {
-        isModalOpen = false;
-        return document.body.classList.remove("modal--open")
-    }
-    isModalOpen = true;
-    document.body.classList += " modal--open"
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
 }
